3ee954a850c2321c50bd710178b29d147ff55f8e
SilverLongjohns <jakehollinger@hotmail.co.uk>
2021-02-02 13:19:34 +0000

    formatting

--- commands/reactions.js
+++ commands/reactions.js
@@ -10,7 +10,7 @@ module.exports = {
 
     let embed = new Discord.MessageEmbed()
     .setTitle("Hello, new member!")
-    .setDescription("To get started, **change your Discord name to your in-game name.**\nKindly follow this format: **[ First Name ] [ Last Name ]**\n*Example: Esmeraude Diamandis*\n`Right-click on your name in the list, and select Change Nickname`\n\nAfterwards, **click on the icon below** to join the rest of the members in the discussions")
+    .setDescription("To get started, **change your Discord name to your in-game name.**\n\nKindly follow this format: **[ First Name ] [ Last Name ]**\n*Example: Esmeraude Diamandis*\n`Right-click on your name in the list, and select Change Nickname`\n\nAfterwards, **click on the icon below** to join the rest of the members in the discussions!\nI hope to see you there!")
 
     let msgEmbed = await message.channel.send(embed)
 
