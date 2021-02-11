c98ac7690b6b67d6c991e39e08e3add0bfdf6082
SilverLongjohns <jakehollinger@hotmail.co.uk>
2021-02-11 15:41:17 +0000

    heroku deployment

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 const Discord = require('discord.js');
 const reactions = require('./commands/reactions');
-const prefix = "!"
-const token = ""
+const prefix = process.env.prefix
+const token = process.env.token
 
 const client = new Discord.Client({ partials: ["MESSAGE", "CHANNEL", "REACTION"]});
 client.commands = new Discord.Collection();
