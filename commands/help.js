d5a60a8889fedcd16522d4ea335a3ff045bc571f
SilverLongjohns <jakehollinger@hotmail.co.uk>
2021-02-11 15:43:55 +0000

    heroku deployment

--- commands/help.js
+++ commands/help.js
@@ -4,6 +4,6 @@ module.exports = {
   name: 'help',
 
   execute (message, args) {
-    message.channel.send("Hello there! Need my help? Here's a list of what I can do for you!\n\n[ Wind-Up Esmeraude Version 1.0 ]\n**!maps** - *Assists in discovering treasure map locations.*\n**!random** - *Generate a random number.*\n**!raffle** - *Holds a raffle or giveaway. (Moderators-only Command)*")
+    message.channel.send("Hello there! Need my help? Here's a list of what I can do for you!\n\n`[ Wind-Up Esmeraude Version 1.0 ]`\n**!maps** - *Assists in discovering treasure map locations.*\n**!random** - *Generate a random number.*\n**!raffle** - *Holds a raffle or giveaway. (Moderators-only Command)*")
 }
 }
